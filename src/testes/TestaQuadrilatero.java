1978f4cfca15d580f9e6b5762d4b054338e24ee1
AmandaJansen <amandagracielly58@gmail.com>
2022-07-24 21:11:43 -0300

    mais um

---
+++ src/testes/TestaQuadrilatero.java
@@ -0,0 +1,16 @@
+package testes;
+
+import classes.Quadrilatero;
+
+public class TestaQuadrilatero {
+
+	public static void main(String[] args) {
+		System.out.println("Exercício quadrilatero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5, 5);
+		Quadrilatero.area(7, 8, 9);
+		Quadrilatero.area(5, 5);
+
+	}
+
+}
